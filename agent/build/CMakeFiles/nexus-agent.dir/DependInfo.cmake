792d0b594af3206555f2c9d261d21ffcb54daf86
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2026-01-26 18:13:19 +0530

    Add agent build binary and update gitignore

---
+++ agent/build/CMakeFiles/nexus-agent.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/collectors/docker_monitor.cpp" "CMakeFiles/nexus-agent.dir/src/collectors/docker_monitor.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/collectors/docker_monitor.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/collectors/log_collector.cpp" "CMakeFiles/nexus-agent.dir/src/collectors/log_collector.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/collectors/log_collector.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/collectors/port_scanner.cpp" "CMakeFiles/nexus-agent.dir/src/collectors/port_scanner.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/collectors/port_scanner.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/collectors/process_scanner.cpp" "CMakeFiles/nexus-agent.dir/src/collectors/process_scanner.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/collectors/process_scanner.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/collectors/security_collector.cpp" "CMakeFiles/nexus-agent.dir/src/collectors/security_collector.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/collectors/security_collector.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/collectors/system_metrics.cpp" "CMakeFiles/nexus-agent.dir/src/collectors/system_metrics.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/collectors/system_metrics.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/communication/http_agent_client.cpp" "CMakeFiles/nexus-agent.dir/src/communication/http_agent_client.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/communication/http_agent_client.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/communication/http_client.cpp" "CMakeFiles/nexus-agent.dir/src/communication/http_client.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/communication/http_client.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/communication/websocket_client.cpp" "CMakeFiles/nexus-agent.dir/src/communication/websocket_client.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/communication/websocket_client.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/detectors/service_detector.cpp" "CMakeFiles/nexus-agent.dir/src/detectors/service_detector.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/detectors/service_detector.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/handlers/command_handler.cpp" "CMakeFiles/nexus-agent.dir/src/handlers/command_handler.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/handlers/command_handler.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/handlers/docker_handler.cpp" "CMakeFiles/nexus-agent.dir/src/handlers/docker_handler.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/handlers/docker_handler.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/handlers/file_handler.cpp" "CMakeFiles/nexus-agent.dir/src/handlers/file_handler.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/handlers/file_handler.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/main.cpp" "CMakeFiles/nexus-agent.dir/src/main.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/main.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/orchestrator/injector.cpp" "CMakeFiles/nexus-agent.dir/src/orchestrator/injector.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/orchestrator/injector.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/orchestrator/instrumentation_manager.cpp" "CMakeFiles/nexus-agent.dir/src/orchestrator/instrumentation_manager.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/orchestrator/instrumentation_manager.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/utils/config.cpp" "CMakeFiles/nexus-agent.dir/src/utils/config.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/utils/config.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/utils/logger.cpp" "CMakeFiles/nexus-agent.dir/src/utils/logger.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/utils/logger.cpp.o.d"
+  "/home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/utils/otlp_converter.cpp" "CMakeFiles/nexus-agent.dir/src/utils/otlp_converter.cpp.o" "gcc" "CMakeFiles/nexus-agent.dir/src/utils/otlp_converter.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ agent/build/CMakeFiles/nexus-agent.dir/src/communication/http_client.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/nexus-agent.dir/src/communication/http_client.cpp.o: \
+ /home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/src/communication/http_client.cpp \
+ /usr/include/stdc-predef.h \
+ /home/jeebanjyoti.mallik@apmosys.mahape/Test/agent/include/nexus/communication/http_client.h
